f62c37cfdaa71f92c717ea8049d62698a0c2e1b6
Sergio Junior <sergio.galdino.junior01@gmail.com>
2026-09-17 14:28:29 -0300

    #DEV: Conclusão do jogo por meio do video

--- frontend/scr/js/script.js
+++ frontend/scr/js/script.js
@@ -1,4 +1,6 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
 
 const jump = () => {
     mario.classList.add('jump');
@@ -8,12 +10,21 @@ const jump = () => {
 };
 
 const loop = setInterval(() => {
-    const pipe = document.querySelector('.pipe');
     const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     
-    if (pipePosition <= 120) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
     }
 }, 10);
 
